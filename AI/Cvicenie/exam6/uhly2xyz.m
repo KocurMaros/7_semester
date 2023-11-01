99cbc3ec26bb1594ea3d03b6e6f8da1769553ee1
maros <kocur.maros@gmail.com>
2023-11-01 23:00:21 +0100

    dokumentovy server

---
+++ AI/Cvicenie/exam6/uhly2xyz.m
@@ -0,0 +1,31 @@
+% Vypocet suradnic [x,y,z] zo vstupnych uhlov u1-u6
+
+function[c]=uhly2xyz(u1,u2,u3,u4,u5,u6)
+u6=0;
+
+
+R1 = 0.188;
+R2 = 1.175; 
+R3 = 1.3; 
+R4 = 0.2;
+R6x = 0.3;
+R6z = 0.1;
+
+A= [cos(u1) -sin(u1) 0 0;sin(u1) cos(u1) 0 0;0 0 1 0;0 0 0 1];
+B=[1 0 0 R1;0 1 0 0;0 0 1 0;0 0 0 1];
+C = [cos(u2) 0 sin(u2) 0;0 1 0 0;-sin(u2) 0 cos(u2) 0;0 0 0 1]; % nova od fera
+D=[1 0 0 0;0 1 0 0;0 0 1 R2;0 0 0 1];
+E = [cos(u3) 0 sin(u3) 0;0 1 0 0;-sin(u3) 0 cos(u3) 0;0 0 0 1]; % nova od fera
+F= [1 0 0 0;0 cos(u4) -sin(u4) 0;0 sin(u4) cos(u4) 0;0 0 0 1];
+G=[1 0 0 R3;0 1 0 0;0 0 1 0;0 0 0 1];
+H= [cos(u5) 0 sin(u5) 0;0 1 0 0;-sin(u5) 0 cos(u5) 0;0 0 0 1];
+I=[1 0 0 R4;0 1 0 0;0 0 1 0;0 0 0 1];
+J=[1 0 0 0;0 cos(u6) -sin(u6) 0;0 sin(u6) cos(u6) 0;0 0 0 1];
+
+%TM = A*B*C*D*E*F*G*H;
+
+%K2= A*B*C*[0;0;R2;1];
+%K3= A*B*C*D*E*F*[R3;0;0;1];
+KB= A*B*C*D*E*F*G*H*I*J*[R6x;0;-R6z;1];
+c=[KB(1:3)];
+
