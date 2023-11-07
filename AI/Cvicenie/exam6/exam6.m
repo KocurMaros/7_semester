c81e9dce059aa60c17733a98a97d20354ed3ff4b
KocurMaros <kocur.maros@gmail.com>
2023-11-07 22:29:34 +0100

    umint 6

--- AI/Cvicenie/exam6/exam6.m
+++ AI/Cvicenie/exam6/exam6.m
@@ -3,8 +3,8 @@ clc
 close all
 tic
 
-lpop = 50;
-numgen=100;
+lpop = 35;
+numgen=5000;
 num_points = 6;
 joints = 6;
 lstring=joints*num_points;
@@ -16,32 +16,47 @@ Delta=Space(2,:)/100;
 Pop=genrpop(lpop,Space);
 
 evolutions=zeros(1,numgen);
-Fit = zeros(numgen,lpop);
+Fit = zeros(1,lpop);
 P0=[1.988; 0; 1.075]; 
 P1=[1; 0; -0.4];
 P2=[0.8; 0;1]; 
 P3=[0; 0.5; 1];
 P4=[-0.75;0.5; 0];
 Points = [P0,P1,P2,P3,P4,P0];
-alfa = 0.5;
+alfa = 0.1;
 end_points = zeros(3,6);
+graph = zeros(3,6);
+evol_sn = zeros(1,numgen);
+evol_pp = zeros(1,numgen);
+step_angles = 0:6:35;
+
 for gen=1:numgen
     for i=1:lpop
+        PP = 0;
+        SN = 0;
+        error_angle = 0;
        for j=1:num_points
             offset = (j-1)*6+1;
             [x,y,z] = uhly2xyz_moja(Pop(i,offset:offset+5));
-            PP = sqrt((Points(1,j)-x)^2+(Points(2,j)-y)^2+(Points(3,j)-z)^2);
-            SN = sum(Pop(i,offset:offset+5));
-            Fit(gen,i) = PP + alfa*SN;
+            PP = PP + sqrt((Points(1,j)-x)^2+(Points(2,j)-y)^2+(Points(3,j)-z)^2);
+            SN = SN + abs(sum(Pop(i,offset:offset+5)));
        end
+       for j = 1:num_points-1
+           for g = 1:num_points
+                error_angle = error_angle + abs(Pop(i,step_angles(j)+g) - Pop(i,step_angles(j+1)+g));
+           end
+       end
+       evol_pp(gen) = PP;
+       evol_sn(gen) = error_angle;
+       Fit(i) = PP + alfa*error_angle;
     end
 
-    evolutions(gen)=min(Fit(gen,:));	% convergence graph of the solution
+    evolutions(gen)=min(Fit);	% convergence graph of the solution
     % GA
-    Best=selbest(Pop,Fit(gen,:),[1,1,1]);
-    Old=selrand(Pop,Fit(gen,:),5);
-    Work1 = selsus(Pop,Fit(gen,:),21);
-    Work2 = selsus(Pop,Fit(gen,:),21);
+    Best=selbest(Pop,Fit,[1,1]);
+    Old=selrand(Pop,Fit,5);
+    Work1 = selsus(Pop,Fit,11);
+    Work2 = selsus(Pop,Fit,17);
     Work1=crossov(Work1,1,0);
     Work2=mutx(Work2,0.1,Space);
     Work2=muta(Work2,0.15,Delta,Space);
@@ -52,10 +67,9 @@ figure(1);
 plot(evolutions);
 xlabel('generation');
 ylabel('fitness');
-hold on 
+% % save("test1.mat");
 figure(2)
-hold on
+plot(evol_pp)
 figure(3)
-hold on
-save("test1.mat");
+plot(evol_sn)
 toc
