3bb43ba2e60b55351ae3f6227bd4fdad11078f3f
maros <kocur.maros@gmail.com>
2023-11-22 21:29:27 +0100

    umint 8 idem robit

--- AI/Cvicenie/exam8/exam8.m
+++ AI/Cvicenie/exam8/exam8.m
@@ -2,44 +2,56 @@ clc
 clear 
 close all
 
-sim_time = 100;
-sim_step = 1/200;
+sim_time = 2500;
+sim_step = 0.1;
 
 u = 33.6;
 
 % [x,t] = simplenarx_dataset;
 out = sim("exam8_sim.slx");
 
-x1 = num2cell((out.w.Data)');
-y1 = num2cell((out.y.Data)');
-t = num2cell((out.w.Time)');
-trainPercentage = 0.8; 
-numTimeSteps = numel(x1);
-numTrain = floor(trainPercentage * numTimeSteps);
+x_train = num2cell((out.w.Data)');
+y_train = num2cell((out.y.Data)');
+
+x_test = num2cell((out.w1.Data)');
+y_test = num2cell((out.y1.Data)');
+% t = num2cell((out.w.Time)');
+% trainPercentage = 0.8; 
+% numTimeSteps = numel(x1);
+% numTrain = floor(trainPercentage * numTimeSteps);
+% Xtrain = x_train(:,1:numTrain);
+% Xtest = x_train(:,numTrain+1:end);
+% Ytrain = y_train(:,1:numTrain);
+% Ytest = y_train(:,numTrain+1:end);
 
 net = narxnet(1:2,1:2,10);
+net.divideFcn = 'dividetrain';
+net.divideParam.testRatio = 0.0;
+net.divideParam.trainRatio = 0.7;
+net.divideParam.valRatio = 0.3;
+net.trainParam.epochs = 500;
 
-[X,Xi,Ai,T] = preparets(net,x1,{},y1);
-
-Xtrain = X(:,1:numTrain);
-Xtest = X(:,numTrain+1:end);
-Ttrain = T(:,1:numTrain);
-Ttest = T(:,numTrain+1:end);
-
+[X,Xi,Ai,T] = preparets(net,x_train,{},y_train);
 net = train(net,X,T,Xi,Ai);
-Y_out = net(Xtest,Xi,Ai);
-% 
+Y_net_train = net(x_test,Xi,Ai);
+% Y_net_test = net(x_test,Xi,Ai);
+
+netc = closeloop(net);
+
+[Xc,Xic,Aic,Tc] = preparets(netc,x_train,{},y_train);
+Y_netc_test = netc(Xc,Xic,Aic);
+% Y_netc_test = net(x_test,Xic,Aic);
+
+gensim(net)
+gensim(netc)
+
+
+% [X,Xi,Ai,T] = preparets(net,x1,{},y1);
 % X_out = Xtest(1,:);
 % X_arr = cell2mat(X_out);
 % Y_arr = cell2mat(Y_out);
 % plot(X_arr,Y_arr, 'b'); hold on; plot(x1,y1, 'r');
 
-netc = closeloop(net);
-[Xc,Xic,Aic,Tc] = preparets(netc,x1,{},y1);
-Yc = netc(Xc,Xic,Aic);
-
-gensim(net)
-gensim(netc)
 % 
 % view(net)
 % Y = net(X,Xi,Ai)
--- IDS/cvika/exam2/exam1.m
+++ IDS/cvika/exam2/exam1.m
@@ -28,13 +28,14 @@ t_model = linspace(0, t(end), 10000);
 y_model = step(F,t_model);
 plot(t_model,y_model)
 %5.1.1 2.metoda 
-ind1 = 73;  %cas 0.7 sekundy 
-ind2 = 36; %cas 0.33 sekundy 
+
+ind1 = find_t(y,0.7); 
+ind2 = find_t(y,0.33);
 
 t1 = t(ind1);
 t2 = t(ind2);
-y1 = y(ind1);
-y2 = y(ind2);
+% y1 = y(ind1);
+% y2 = y(ind2);
 
 K = y(end);
 T = 1.245*(t1-t2);
@@ -46,8 +47,8 @@ y_model = step(F,t_model);
 plot(t_model,y_model)
 
 %5.1.1 3.metoda 
-ind1 = find_t(y,0.7); 
-ind2 = find_t(y,0.33);
+ind1 = find_t(y,0.4); 
+ind2 = find_t(y,0.28);
 t1 = t(ind1);
 t2 = t(ind2);
 
@@ -64,3 +65,4 @@ t_model = linspace(0, t(end), 10000);
 y_model = step(F,t_model);
 plot(t_model,y_model)
 legend("namerane","prva metoda","druha metoda","tretia metoda")
+
