f86b5761044072d2837d8bdcec19b826105eac7d
maros <kocur.maros@gmail.com>
2023-11-23 18:04:03 +0100

    9 tyzden koniec skoly

--- AI/Cvicenie/exam8/exam8.m
+++ AI/Cvicenie/exam8/exam8.m
@@ -10,7 +10,7 @@ sim_step = 0.01;
 u = 33.6;
 
 % [x,t] = simplenarx_dataset;
-out = sim("exam8_sim.slx");
+out = sim("exam8_sim23a.slx");
 
 x_train = num2cell((out.w.Data)');
 y_train = num2cell((out.y.Data)');
@@ -18,13 +18,15 @@ y_train = num2cell((out.y.Data)');
 % x_test = num2cell((out.w1.Data)');
 % y_test = num2cell((out.y1.Data)');
 
-trainPercentage = 0.8; 
-numTimeSteps = numel(x_train);
-numTrain = floor(trainPercentage * numTimeSteps);
-Xtrain = x_train(:,1:numTrain);
-Xtest = x_train(:,numTrain+1:end);
-Ytrain = y_train(:,1:numTrain);
+% trainPercentage = 0.8; 
+% numTimeSteps = numel(x_train);
+% numTrain = floor(trainPercentage * numTimeSteps);
+% Xtrain = x_train(:,1:numTrain);
+% Xtest = x_train(:,numTrain+1:end);
+% Ytrain = y_train(:,1:numTrain);
 
+Xtrain = x_train;
+Ytrain = y_train;
 net = narxnet(1:2,1:2,10);
 net.divideFcn = 'dividetrain';
 
@@ -40,10 +42,32 @@ figure(1)
 hold on
 plot(out.y.Data,'b')
 plot(y_plot,'r');
-
+xlabel("vzorky [pocet]")
+ylabel("y oyput")
+title("Trenovacie data");
 %%%%%%%%%%close loop
 x_train = num2cell((out.w1.Data)');
 y_train = num2cell((out.y1.Data)');
+
+
+
+Xtest = x_train;
+Ytest = y_train;
+
+[Xc,Xic,Aic,Tc] = preparets(net,Xtest,{},Ytest);
+Yc = net(Xc,Xic,Aic);
+y_plotcc = cell2mat(Yc);
+
+figure(3)
+hold on
+vstupne =out.y1.Data;
+plot(vstupne,'b');
+plot(y_plotcc,'g');
+xlabel("vzorky [pocet]")
+ylabel("y oyput")
+title("Testovanie open loop siete na inom datasete");
+
+netc = closeloop(net);
 
 trainPercentage = 0.8; 
 numTimeSteps = numel(x_train);
@@ -51,6 +75,23 @@ numTrain = floor(trainPercentage * numTimeSteps);
 Xtrain = x_train(:,1:numTrain);
 Xtest = x_train(:,numTrain+1:end);
 Ytrain = y_train(:,1:numTrain);
+
+[Xc,Xic,Aic,Tc] = preparets(netc,Xtrain,{},Ytrain);
+Yc = netc(Xtest,Xic,Aic);
+y_plotcc = cell2mat(Yc);
+
+figure(2)
+hold on
+vstupne =out.y1.Data;
+offsetnute = vstupne(20000:end);
+plot(offsetnute,'b');
+plot(y_plotcc,'g');
+xlabel("vzorky [pocet]")
+ylabel("y oyput")
+title("Testovanie close loop siete na inom datasete");
+
+% gensim(net,sim_step)
+% gensim(netc,sim_step)
 
 netc = closeloop(net);
 
@@ -64,7 +105,9 @@ vstupne =out.y1.Data;
 offsetnute = vstupne(20000:end);
 plot(offsetnute,'b');
 plot(y_plotcc,'g');
-
+xlabel("vzorky [pocet]")
+ylabel("y oyput")
+title("Testovanie close loop siete na inom datasete");
 
 % gensim(net,sim_step)
 % gensim(netc,sim_step)
